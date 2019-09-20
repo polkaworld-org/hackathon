a54cf53cd38d32d4d2028b139fdb4a62e6df3d40
yznt <nsso@foxmail.com>
2019-09-20 16:06:10 +0800

    update

--- src/Account.js
+++ src/Account.js
@@ -1,6 +1,12 @@
 import { Keyring } from '@polkadot/api';
 import config from './config';
 
+let currentAccount = getAccountFromStorage();
+
+window.addEventListener('storage', function(e) {
+  currentAccount = getAccountFromStorage();
+});
+
 export { mnemonicValidate } from '@polkadot/util-crypto';
 
 export function mnemonicExist(mnemonic) {
@@ -20,4 +26,25 @@ export function saveAccount(mnemonic) {
       mnemonic,
     })
   );
+}
+
+export function getAccountFromStorage() {
+  const _account = localStorage.getItem('currentAccount');
+
+  if (!_account) return null;
+
+  const account = JSON.parse(_account);
+
+  const configAccount = config.find(a => a.address === account.address);
+
+  if (configAccount.mnemonic !== account.mnemonic) {
+    localStorage.setItem('currentAccount', '');
+    return null;
+  }
+
+  return account;
+}
+
+export function getAccount() {
+  return currentAccount;
 }
--- src/Home.js
+++ src/Home.js
@@ -1,94 +1,12 @@
-import React, { useState, useMemo } from 'react';
+import React from 'react';
 
-import { makeStyles } from '@material-ui/styles';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-
-import logo from './imgs/logo.jpg';
-
-import { mnemonicValidate, mnemonicExist, saveAccount } from './Account';
-
-const useStyles = makeStyles({
-  container: {
-    paddingTop: 56,
-    display: 'flex',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  imgContainer: {
-    paddingLeft: 60,
-    paddingRight: 60,
-    '& img': {
-      width: '100%',
-      maxWidth: 480,
-    },
-  },
-  textFieldContainer: {
-    marginTop: 8,
-    display: 'inline-flex',
-    width: '100%',
-    maxWidth: 560,
-    padding: 16,
-    paddingBottom: 32,
-  },
-  textfield: {
-    width: '100%',
-  },
-  button: {
-    height: 48,
-    width: 163,
-  },
-});
+import { getAccount } from './Account';
 
 export default function() {
-  const classes = useStyles();
+  // const classes = useStyles();
+  const account = getAccount();
 
-  const [mnemonic, setMnemonic] = useState('');
-  const [errorMsg, setErrorMsg] = useState('');
+  console.log(account)
 
-  return (
-    <div className={classes.container}>
-      <div className={classes.imgContainer}>
-        <img src={logo} alt="polkaworld hackathon" />
-      </div>
-      <div className={classes.textFieldContainer}>
-        <TextField
-          className={classes.textfield}
-          multiline
-          rows="2"
-          rowsMax="100"
-          value={mnemonic}
-          helperText={errorMsg}
-          placeholder="Import Mnemonic"
-          onChange={e => setMnemonic(e.target.value)}
-          margin="normal"
-          variant="outlined"
-          error={!!errorMsg}
-          onFocus={() => setErrorMsg('')}
-        />
-      </div>
-      <div>
-        <Button
-          className={classes.button}
-          variant="contained"
-          size="large"
-          color="primary"
-          onClick={() => {
-            if (!mnemonic) {
-              setErrorMsg('please input mnemonic');
-            } else if (!mnemonicValidate(mnemonic.trim())) {
-              setErrorMsg('mnemonic error');
-            } else if (!mnemonicExist(mnemonic.trim())) {
-              setErrorMsg('account not exist');
-            } else {
-              saveAccount(mnemonic.trim());
-            }
-          }}
-        >
-          LOGIN
-        </Button>
-      </div>
-    </div>
-  );
+  return <div ></div>;
 }
