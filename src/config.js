7f5d9a20069652164115c463b4074b1b553b47f3
yznt <nsso@foxmail.com>
2019-09-22 12:01:10 +0800

    update

--- src/config.js
+++ src/config.js
@@ -65,93 +65,8 @@ export default [
   },
   {
     type: '2',
-    projectName: 'Trisolarans',
-    address: '5HQkfXh5StLCyTSn6fKU8gBxrjLUTFLd7KGSYuw2WyC1ggJQ',
-  },
-  {
-    type: '2',
-    projectName: 'Sky',
-    address: '5Cf2azxagvqSZcHezAtQmL7ddsgAum6bQ1bUH9AdPrLLAtbV',
-  },
-  {
-    type: '2',
-    projectName: 'Rebase',
-    address: '5Hi7jxMRC36kWcwgobhPfYLsZomvSZ1j1cfQDRXtyMHuwWtD',
-  },
-  {
-    type: '2',
-    projectName: '996',
-    address: '5EQqqr2hi5CZvyfdXe13PeHKujBu89MSR4ijJw5wzmSFQ3xa',
-  },
-  {
-    type: '2',
-    projectName: '链人',
-    address: '5HNdwG6X9edbuHE9htoqqECjhFTd5cDU6ma8hZy4SGRvHSFN',
-  },
-  {
-    type: '2',
-    projectName: 'A.T.Dot',
-    address: '5FK5fVWYPz6AoP5VUgu9JX45uMdYEtEwiFn3886CqPiZnFoM',
-  },
-  {
-    type: '2',
-    projectName: 'Pacman',
-    address: '5GUJRFajJEmajZbYr2Hro9NBv8ZQ1AnbrKnYWkAqHACEnXQP',
-  },
-  {
-    type: '2',
-    projectName: 'altice.io',
-    address: '5D6TAoRVzc9bQPRFYXtVyT87eacr3RvrG6RyuAap1h5NMx7J',
-  },
-  {
-    type: '2',
-    projectName: '南斗神拳',
-    address: '5F2SZZexT1qy23Gri9N2zudCMAnwn9rZKEBvZv36wj6drGnS',
-  },
-  {
-    type: '2',
-    projectName: '不知名团队',
-    address: '5Gvxz1RZSCXz1X4s9M8ViPHzT4Qg6skFyVHqje1naxE5Dodw',
-  },
-  {
-    type: '2',
     projectName: '小明的同学们',
     address: '5EMFxyLX7ShshT2yGSPB21ohDphzE4oiDfJzcxQXqPucxuuk',
-  },
-  {
-    type: '2',
-    projectName: 'Polkawallet',
-    address: '5EAqix4j1Utuwj6XDDEeK4isoB4tZuZSx7o44ic7RKyHXzeV',
-  },
-  {
-    type: '2',
-    projectName: 'abmatrix',
-    address: '5DPcxhcZiVCDE83bUWYrk8bpWfSJ7qG7t3JAoosCE5yjE7gC',
-  },
-  {
-    type: '2',
-    projectName: 'Apollo',
-    address: '5G8k8tPTgYprA9yrxFcR14qAk6kxkvn3snofbv5r4D2cQ2oq',
-  },
-  {
-    type: '2',
-    projectName: '42',
-    address: '5CCigkARvdUut8FfZRWUST4SfPFjw9wNi2wYDVhczbqQzDDo',
-  },
-  {
-    type: '2',
-    projectName: 'Bifrost',
-    address: '5EzKn4VM3sk8mxaNnH7RBuYfwQdEJ27owtm26PvRTcpfKZQz',
-  },
-  {
-    type: '2',
-    projectName: 'Data Guard',
-    address: '5GPm2m6MwVqQLsUbrJJ9MCCYZomxYBDVuDbYpQn5jgeUzssP',
-  },
-  {
-    type: '2',
-    projectName: 'BitRocket',
-    address: '5H7SSNo2jjfCphZsgoMK1KZXhkLXSiZvfAjaUdFchQw7U4ms',
   },
   {
     type: '2',
@@ -160,8 +75,18 @@ export default [
   },
   {
     type: '2',
-    projectName: 'Funsole',
-    address: '5HFKMyCrZhzPUb8sdRFgDJCJhqpN3HVsQJyGwV5X6W83VVSh',
+    projectName: 'Polkawallet',
+    address: '5EAqix4j1Utuwj6XDDEeK4isoB4tZuZSx7o44ic7RKyHXzeV',
+  },
+  {
+    type: '2',
+    projectName: 'Sky',
+    address: '5Cf2azxagvqSZcHezAtQmL7ddsgAum6bQ1bUH9AdPrLLAtbV',
+  },
+  {
+    type: '2',
+    projectName: '42',
+    address: '5CCigkARvdUut8FfZRWUST4SfPFjw9wNi2wYDVhczbqQzDDo',
   },
   {
     type: '2',
@@ -170,17 +95,79 @@ export default [
   },
   {
     type: '2',
-    projectName: '吴勃',
-    address: '5DahsxkAD4PKptNRq1GMgucTctiMDnAQCExVP1Sju22dxgoQ',
+    projectName: 'Bifrost',
+    address: '5EzKn4VM3sk8mxaNnH7RBuYfwQdEJ27owtm26PvRTcpfKZQz',
   },
   {
     type: '2',
-    projectName: '寄意兰舟',
-    address: '5Cq2Zozk9nHxyxRYjnkq5rBi3TPEGiJbtXxQnYgEPc95NZX1',
+    projectName: '链人',
+    address: '5HNdwG6X9edbuHE9htoqqECjhFTd5cDU6ma8hZy4SGRvHSFN',
   },
   {
     type: '2',
     projectName: 'fanyang',
     address: '5F5w2FNwVAeXTR5HvmpCD9jBCyVHBUoyGdyyXdfzSGC7XBz7',
+  },
+  {
+    type: '2',
+    projectName: '吴勃',
+    address: '5DahsxkAD4PKptNRq1GMgucTctiMDnAQCExVP1Sju22dxgoQ',
+  },
+  {
+    type: '2',
+    projectName: 'Apollo',
+    address: '5G8k8tPTgYprA9yrxFcR14qAk6kxkvn3snofbv5r4D2cQ2oq',
+  },
+  {
+    type: '2',
+    projectName: '不知名团队',
+    address: '5Gvxz1RZSCXz1X4s9M8ViPHzT4Qg6skFyVHqje1naxE5Dodw',
+  },
+  {
+    type: '2',
+    projectName: 'Pacman',
+    address: '5GUJRFajJEmajZbYr2Hro9NBv8ZQ1AnbrKnYWkAqHACEnXQP',
+  },
+
+  {
+    type: '2',
+    projectName: 'BitRocket',
+    address: '5H7SSNo2jjfCphZsgoMK1KZXhkLXSiZvfAjaUdFchQw7U4ms',
+  },
+  {
+    type: '2',
+    projectName: 'Funsole',
+    address: '5HFKMyCrZhzPUb8sdRFgDJCJhqpN3HVsQJyGwV5X6W83VVSh',
+  },
+  {
+    type: '2',
+    projectName: 'altice.io',
+    address: '5D6TAoRVzc9bQPRFYXtVyT87eacr3RvrG6RyuAap1h5NMx7J',
+  },
+  {
+    type: '2',
+    projectName: 'Trisolarans',
+    address: '5HQkfXh5StLCyTSn6fKU8gBxrjLUTFLd7KGSYuw2WyC1ggJQ',
+  },
+  {
+    type: '2',
+    projectName: 'Rebase',
+    address: '5Hi7jxMRC36kWcwgobhPfYLsZomvSZ1j1cfQDRXtyMHuwWtD',
+  },
+
+  {
+    type: '2',
+    projectName: 'A.T.Dot',
+    address: '5FK5fVWYPz6AoP5VUgu9JX45uMdYEtEwiFn3886CqPiZnFoM',
+  },
+  {
+    type: '2',
+    projectName: 'FOSS-南斗神拳',
+    address: '5F2SZZexT1qy23Gri9N2zudCMAnwn9rZKEBvZv36wj6drGnS',
+  },
+  {
+    type: '2',
+    projectName: 'abmatrix',
+    address: '5DPcxhcZiVCDE83bUWYrk8bpWfSJ7qG7t3JAoosCE5yjE7gC',
   },
 ];
